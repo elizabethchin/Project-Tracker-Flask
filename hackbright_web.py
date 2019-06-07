ef3eafeef64dc11ad9f1e80a37d274492a65979f
Hackbright Student <no-reply@hackbrightacademy.com>
2019-06-06 19:12:10 -0700

    add /student_add

--- hackbright_web.py
+++ hackbright_web.py
@@ -1,6 +1,6 @@
 """A web application for tracking projects, students, and student grades."""
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 
 import hackbright
 
@@ -11,11 +11,48 @@ app = Flask(__name__)
 def get_student():
     """Show information about a student."""
 
-    github = "jhacks"
+    github = request.args.get("github")
 
     first, last, github = hackbright.get_student_by_github(github)
 
-    return "{} is the GitHub account for {} {}".format(github, first, last)
+    title = hackbright.get_grades_by_github(github)
+
+    html = render_template("student_info.html",
+                            first=first,
+                            last=last,
+                            github=github,
+                            title=title)
+
+    return html
+
+@app.route("/student-search")
+def get_student_form():
+    """Show form for searching for a student."""
+
+    return render_template("student_search.html")
+
+@app.route("/add_student")
+def add_student():
+    """Displays a form to add student."""
+    #this allows you to add student in the html file
+    #which links you to "/student-add"
+    #this info is inserted to "/student-add" function
+
+    return render_template("student_add.html")
+
+@app.route("/student-add", methods=['POST'])
+def student_add():
+    """Add a student."""
+
+    first = request.form.get("first_name")#must match html form
+    last = request.form.get("last_name")
+    github = request.form.get("github")
+    #uses function that adds student to DB
+    hackbright.make_new_student(first, last, github)
+
+    #shows page of student info, can really return to any page you want
+    return redirect("/student?github=" + github)
+
 
 if __name__ == "__main__":
     hackbright.connect_to_db(app)
